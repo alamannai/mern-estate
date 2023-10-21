92f9d523fadbbcad13395ec4e2e6f5ebb290a60f
Ala Mannai <mannai17@outlook.fr>
2023-10-22 01:03:51 +0200

    connect-db

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom"
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <div className='flex justify-between items-center max-w-full mx-auto p-3'>
             <Link to={'/'}>
-                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap ml-3'>
                     <span className='text-slate-500'>Phantom</span>
                     <span className='text-slate-700'>Estate</span>
                 </h1>
@@ -19,7 +19,7 @@ export default function Header() {
                 />
                 <FaSearch className='text-slate-500' />
             </form>
-            <ul className='flex gap-4'>
+            <ul className='flex gap-4 mr-5'>
                 <Link to={'/'}>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
                 </Link>
