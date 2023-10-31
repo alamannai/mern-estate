29bb12328e9d2c2af7b02b83b0d02199c3db0f13
Ala Mannai <mannai17@outlook.fr>
2023-10-31 22:59:55 +0100

    complete-signup

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -6,14 +6,14 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    password :{
+        type: String,
+        required: true,
+    },
     email :{
         type: String,
         required: true,
         unique: true,
-    },
-    password :{
-        type: String,
-        required: true,
     }
 },{timestamps: true});
 
